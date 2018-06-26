4a79e8c9b24d53a5b7131f22ac1702d3a347bb60
Delmy <cdelmy71@gmail.com>
2018-06-26 14:47:41 -0600

    Creacion de el formulario de iniciar sesion.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.toggle').click(function () {
+    $('.formulario').animate({
+      heigth:"toggle",
+        'padding-top':'toggle',
+        'padding-bottom': 'toggle',
+        opacity: 'toggle'
+
+    }, "slow");
+
+});
